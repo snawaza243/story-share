e342de278d1d788445416596491a493dd32eb933
Shahnawaz Alam <91892524+snawaza243@users.noreply.github.com>
2023-02-26 21:47:37 +0530

    deploy v0.1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import './header.css'
 function Header() {
+
+  var i = 0;
+  var txt = 'Welcome to the story bank!'; /* The text */
+  var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("demo").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
   return (
     <>
       <div className="header">
         <div className="headerTitles">
+ 
+
           <span className='headerTitleSm'>Welcome to the story bank</span>
           <span className="headerTitleLg">Daily Story Space</span>
         </div>
-       <center> <img className='headerImg' src="https://res.cloudinary.com/dwqgr4ovb/image/upload/v1675865589/shareyourstory/2-min_uavt9n.png" alt="hacker pic" /></center>
+        <center> <img className='headerImg' src="https://res.cloudinary.com/dwqgr4ovb/image/upload/v1675865589/shareyourstory/2-min_uavt9n.png" alt="hacker pic" /></center>
       </div>
     </>
   )
